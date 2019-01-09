1bf0ab0cf57c7b9d57ac7c8f37dd931d7d710f93
Gaurav sharma <gaurav89110@gmail.com>
2019-01-09 23:02:46 +0530

    Create func_in_func.py

---
+++ gauravrock/func_in_func.py
@@ -0,0 +1,27 @@
+# Define echo_shout()
+def echo_shout(word):
+    """Change the value of a nonlocal variable"""
+    
+    # Concatenate word with itself: echo_word
+    echo_word=word+word
+    
+    # Print echo_word
+    print(echo_word)
+    
+    # Define inner function shout()
+    def shout():
+        """Alter a variable in the enclosing scope"""    
+        # Use echo_word in nonlocal scope
+        nonlocal echo_word
+        
+        # Change echo_word to echo_word concatenated with '!!!'
+        echo_word = echo_word + "!!!"
+    
+    # Call function shout()
+    shout()
+    
+    # Print echo_word
+    print(echo_word)
+
+# Call function echo_shout() with argument 'hello'
+echo_shout('hello')
