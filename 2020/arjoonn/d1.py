eaa6c18630a5ac6d510d16cd1e813bc376fc5021
arjoonn sharma <arjoonn.94@gmail.com>
2020-08-07 11:02:38 +0530

    day1

--- 2020/arjoonn/d1.py
+++ 2020/arjoonn/d1.py
@@ -1,1 +1,16 @@
 # https://leetcode.com/problems/brick-wall/
+
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        rowsums = {}
+        most_gaps = 0
+        for row in wall:
+            total = 0
+            for i in row[:-1]:  # Skip the last brick
+                total += i
+                if total not in rowsums:
+                    rowsums[total] = 0
+                rowsums[total] += 1
+                most_gaps = max((rowsums[total]), most_gaps)
+        return len(wall) - most_gaps
